949bf100e7f59ed05e6e12b8083aeb3050a249aa
Javier Chacón <j.chacon.alvarez@gmail.com>
2019-04-02 11:26:53 +0200

    initial commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -24,24 +24,16 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
+      <main
+        style={{
+          width: `100%`,
+          height: `100vh`,
+          display: `flex`,
+          justifyContent: `center`,
+          alignItems: `center`
+        }}>
+        {children}
+      </main>
     )}
   />
 )
