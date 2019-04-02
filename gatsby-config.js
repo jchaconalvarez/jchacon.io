4bffad8da80dc1d20d47fa31bcf5666450a8f18a
Javier Chacón <j.chacon.alvarez@gmail.com>
2019-04-02 12:39:32 +0200

    added styled components, changed site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `jchacon.io`,
+    description: `English philologist turned web developer. Tech nerd with a knack for problem solving.`,
+    author: `Javier Chacón`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
