1c61512e2496f84aaad5aa9326183fef73e4b435
Javier Chacón <javier.chacon@goldenspear.com>
2019-10-09 15:44:19 +0200

    changed line-height and added stack to resource list

--- src/pages/courses/introduction.js
+++ src/pages/courses/introduction.js
@@ -21,47 +21,49 @@ const Introduction = () => (
         <SectionTitle title="Resources" pullSize={2} />
         <Center>
           <List bullets>
-            <li>
-              <ExternalLink href="http://info.cern.ch/hypertext/WWW/TheProject.html">
-                The World's first web page
-              </ExternalLink>
-              <p>Take a look at the world's first webpage, made in 1991!</p>
-            </li>
-            <li>
-              <ExternalLink href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web">
-                MDN: Getting Started with the Web
-              </ExternalLink>
-              <p>
-                <i>Getting started with the Web</i> is a concise series introducing you to the practicalities of web
-                development.
-              </p>
-            </li>
-            <li>
-              <ExternalLink href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics#Anatomy_of_an_HTML_element">
-                MDN: Anatomy of an HTML element.
-              </ExternalLink>
-              What does an HTML element look like? You can find out here.
-            </li>
-            <li>
-              <ExternalLink href="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5">
-                MDN: HMTL5
-              </ExternalLink>
-              <p>All the information about HTML5 you will ever need.</p>
-            </li>
-            <li>
-              <ExternalLink href="http://www.html5doctor.com">HTML Doctor</ExternalLink>
-              <p>Great resource for getting information about the different HTML elements.</p>
-            </li>
-            <li>
-              <ExternalLink href="https://codeburst.io/block-level-and-inline-elements-the-difference-between-div-and-span-2f8502c1f95b">
-                Block-level and Inline elements: The Difference Between &lt;div&gt; and &lt;span&gt;.
-              </ExternalLink>
-              <p>What's the difference between a Block-level element and an Inline element?</p>
-            </li>
-            <li>
-              <ExternalLink href="https://codepen.io/freeCodeCamp/full/zNqgVx">EXE: Tribute page</ExternalLink>
-              <p>Example tribute page.</p>
-            </li>
+            <Stack>
+              <li>
+                <ExternalLink href="http://info.cern.ch/hypertext/WWW/TheProject.html">
+                  The World's first web page
+                </ExternalLink>
+                <p>Take a look at the world's first webpage, made in 1991!</p>
+              </li>
+              <li>
+                <ExternalLink href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web">
+                  MDN: Getting Started with the Web
+                </ExternalLink>
+                <p>
+                  <i>Getting started with the Web</i> is a concise series introducing you to the practicalities of web
+                  development.
+                </p>
+              </li>
+              <li>
+                <ExternalLink href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics#Anatomy_of_an_HTML_element">
+                  MDN: Anatomy of an HTML element.
+                </ExternalLink>
+                <p>What does an HTML element look like? You can find out here.</p>
+              </li>
+              <li>
+                <ExternalLink href="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5">
+                  MDN: HMTL5
+                </ExternalLink>
+                <p>All the information about HTML5 you will ever need.</p>
+              </li>
+              <li>
+                <ExternalLink href="http://www.html5doctor.com">HTML Doctor</ExternalLink>
+                <p>Great resource for getting information about the different HTML elements.</p>
+              </li>
+              <li>
+                <ExternalLink href="https://codeburst.io/block-level-and-inline-elements-the-difference-between-div-and-span-2f8502c1f95b">
+                  Block-level and Inline elements: The Difference Between &lt;div&gt; and &lt;span&gt;.
+                </ExternalLink>
+                <p>What's the difference between a Block-level element and an Inline element?</p>
+              </li>
+              <li>
+                <ExternalLink href="https://codepen.io/freeCodeCamp/full/zNqgVx">EXE: Tribute page</ExternalLink>
+                <p>Example tribute page.</p>
+              </li>
+            </Stack>
           </List>
         </Center>
       </Stack>
