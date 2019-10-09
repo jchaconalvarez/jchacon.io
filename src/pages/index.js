c0aa59b3c9ce12960f0f098f66cb4b0387701051
Javier Chacón <j.chacon.alvarez@gmail.com>
2019-10-09 21:56:29 +0200

    added helmet with title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 import { Layout, SEO } from '../components'
 
 const IndexPage = () => (
   <Layout>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>jchacon.io</title>
+      <link rel="canonical" href="https://www.jchacon.io" />
+    </Helmet>
     <SEO
       title="jchacon.io"
       keywords={['portfolio', 'web development', 'javier chacon']}
