cd26272a2c4b26a85356126e22262ff9bfc53031
Javier Chacón <javier.chacon@goldenspear.com>
2019-10-09 16:00:19 +0200

    centered and changed fontfamily landing h1

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,9 @@ const IndexPage = () => (
   <Layout>
     <SEO title="" keywords={['portfolio', 'web development', 'javier chacon']} />
     <React.StrictMode>
-      <h1>jchacon.io</h1>
+      <div style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <h1 style={{ fontFamily: 'Ubuntu Mono' }}>jchacon.io</h1>
+      </div>
     </React.StrictMode>
   </Layout>
 )
