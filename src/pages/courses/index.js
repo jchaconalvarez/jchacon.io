5c6dda9acd1d62cbbd6444a8588d3d9cb90f143d
Javier Chacón <j.chacon.alvarez@gmail.com>
2019-10-15 18:27:03 +0200

    added day 4 link

--- src/pages/courses/index.js
+++ src/pages/courses/index.js
@@ -31,8 +31,17 @@ const Courses = () => {
               number="3"
               text="Flexbox"
             />
-            {/* <LessonListItem before="Week 2" linkTo="/courses/flexbox" number="4" text="Flexbox" />  */}
-            {/* <LessonListItem linkTo="/courses/responsive" number="5" text="Responsive Design" /> */}
+            <LessonListItem
+              before="Week 2"
+              linkTo="/courses/responsive"
+              number="4"
+              text="Responsive Design"
+            />
+            {/* <LessonListItem
+              linkTo="/courses/responsive"
+              number="5"
+              text="Responsive Design"
+            /> */}
             {/* <LessonListItem linkTo="/courses/portfolio" number="6" text="Portfolio" /> */}
             {/* <LessonListItem before="Week 3" linkTo="/courses/portfolio" number="7" text="Portfolio" /> */}
             {/* <LessonListItem linkTo="/courses/portfolio" number="8" text="Portfolio" /> */}
