e720db2b6bac9b898ac660593812d92dd137767c
Javier Chacón <j.chacon.alvarez@gmail.com>
2020-06-29 21:34:43 +0200

    installed styled-components, transition-link

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `jchacon.io`,
+    description: ``,
+    author: `@JCHaAlv`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-transition-link`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const About = () => {
+  return (
+    <Layout>
+      <h1>ABOUT</h1>
+    </Layout>
+  )
+}
+
+export default About
