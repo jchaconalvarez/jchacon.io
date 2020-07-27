bc2fdeb8f1986e08dad5013505ab603bd4a0f807
Javier Chacón <j.chacon.alvarez@gmail.com>
2020-07-27 20:52:24 +0200

    initial setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `jchacon.io`,
-    description: ``,
-    author: `@JCHaAlv`,
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-styled-components`,
-    `gatsby-plugin-fontawesome-css`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -20,8 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `jchacon-io`,
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
         start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,22 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from "react"
+import { Link } from "gatsby"
 
-import { ContactForm, Layout, SEO, Social } from '../components'
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
 
-const AutoType = ({ string }) => {
-  const autoTypeRef = useRef(null)
-
-  let i = 0
-
-  const type = () => {
-    console.log('auto: ', autoTypeRef)
-    autoTypeRef.current.innerText += string.charAt(i)
-    i++
-    setTimeout(type, 50)
-  }
-
-  return autoTypeRef && <span ref={autoTypeRef}>{type()}</span>
-}
-
-const IndexPage = () => {
-  return (
-    <Layout>
-      <SEO title="Home" />
-      <h1>HOME</h1>
-      <AutoType string={'This is a test'} />
-      <p>
-        You probably haven't heard of them chillwave synth stumptown man braid
-        slow-carb fam woke taxidermy four loko. Forage wayfarers 90's photo
-        booth. Umami af pork belly everyday carry, occupy YOLO four loko austin
-        mustache pinterest vexillologist. Banh mi roof party gentrify, whatever
-        asymmetrical thundercats polaroid organic keffiyeh air plant glossier
-        chambray. Helvetica wayfarers chia, sartorial cred poke shabby chic
-        hoodie 3 wolf moon ramps thundercats. Disrupt venmo iPhone fixie cronut
-        post-ironic ugh meh DIY drinking vinegar jean shorts.
-      </p>
-      <h2>CONTACT</h2>
-      <Social />
-      <ContactForm />
-    </Layout>
-  )
-}
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link> <br />
+    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+  </Layout>
+)
 
 export default IndexPage
